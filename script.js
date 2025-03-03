89aaad1574550a39da8d60a2123410636f15d3e7
Bruna Lima <133261652+brunatlrocha@users.noreply.github.com>
2025-03-02 23:48:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-anime({
-    targets: '#perfil img',
-    translateX: 250,
-    easing: 'easeInOutQuad',
-    duration: 1000
-});
-
-function searchFunction() {
-    let input = document.getElementById('search').value.toLowerCase();
-
-    items.forEach(item => {
-        if (item.textContent.toLowerCase().includes(input)) {
-            item.style.display = "block"; // Mostra o item que corresponde à pesquisa
-        } else {
-            item.style.display = "none"; // Esconde os itens que não correspondem
-        }
-    });
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
 }
